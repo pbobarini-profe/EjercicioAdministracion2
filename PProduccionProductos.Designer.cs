e742a06d8f35611ed90d998d1857669385fd20db
rdgzssss <rodrigodpz01@hotmail.com>
2025-11-05 23:53:31 -0300

    Avance3

--- PProduccionProductos.Designer.cs
+++ PProduccionProductos.Designer.cs
@@ -31,12 +31,185 @@
 
         private void InitializeComponent()
         {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.dgvProduccion = new System.Windows.Forms.DataGridView();
+            this.gbDatos = new System.Windows.Forms.GroupBox();
+            this.gbListado = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduccion)).BeginInit();
+            this.gbDatos.SuspendLayout();
+            this.gbListado.SuspendLayout();
             this.SuspendLayout();
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "PProduccionProductos";
-            this.Load += new System.EventHandler(this.PProduccionProductos_Load);
-            this.ResumeLayout(false);
 
+            
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.Location = new System.Drawing.Point(220, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(300, 26);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Producción de Productos";
+
+            
+            this.gbDatos.Controls.Add(this.lblId);
+            this.gbDatos.Controls.Add(this.txtId);
+            this.gbDatos.Controls.Add(this.lblFecha);
+            this.gbDatos.Controls.Add(this.dtpFecha);
+            this.gbDatos.Controls.Add(this.lblProducto);
+            this.gbDatos.Controls.Add(this.cmbProducto);
+            this.gbDatos.Controls.Add(this.lblCantidad);
+            this.gbDatos.Controls.Add(this.txtCantidad);
+            this.gbDatos.Controls.Add(this.btnInsertar);
+            this.gbDatos.Controls.Add(this.btnActualizar);
+            this.gbDatos.Controls.Add(this.btnEliminar);
+            this.gbDatos.Controls.Add(this.btnLimpiar);
+            this.gbDatos.Location = new System.Drawing.Point(20, 60);
+            this.gbDatos.Name = "gbDatos";
+            this.gbDatos.Size = new System.Drawing.Size(350, 350);
+            this.gbDatos.TabIndex = 1;
+            this.gbDatos.TabStop = false;
+            this.gbDatos.Text = "Datos de Producción";
+
+            
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(20, 30);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(21, 13);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "ID:";
+
+           
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(120, 27);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(200, 20);
+            this.txtId.TabIndex = 1;
+
+            
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(20, 70);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(40, 13);
+            this.lblFecha.TabIndex = 2;
+            this.lblFecha.Text = "Fecha:";
+
+            this.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFecha.Location = new System.Drawing.Point(120, 67);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 3;
+
+             
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(20, 110);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(53, 13);
+            this.lblProducto.TabIndex = 4;
+            this.lblProducto.Text = "Producto:";
+
+            
+            this.cmbProducto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Location = new System.Drawing.Point(120, 107);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(200, 21);
+            this.cmbProducto.TabIndex = 5;
+
+            
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(20, 150);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(52, 13);
+            this.lblCantidad.TabIndex = 6;
+            this.lblCantidad.Text = "Cantidad:";
+
+            
+            this.txtCantidad.Location = new System.Drawing.Point(120, 147);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(200, 20);
+            this.txtCantidad.TabIndex = 7;
+
+             
+            this.btnInsertar.Location = new System.Drawing.Point(20, 200);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 30);
+            this.btnInsertar.TabIndex = 8;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+
+           
+            this.btnActualizar.Location = new System.Drawing.Point(110, 200);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 30);
+            this.btnActualizar.TabIndex = 9;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+
+            
+            this.btnEliminar.Location = new System.Drawing.Point(200, 200);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 30);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+
+            
+            this.btnLimpiar.Location = new System.Drawing.Point(20, 250);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 30);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+
+            
+            this.gbListado.Controls.Add(this.dgvProduccion);
+            this.gbListado.Location = new System.Drawing.Point(390, 60);
+            this.gbListado.Name = "gbListado";
+            this.gbListado.Size = new System.Drawing.Size(500, 450);
+            this.gbListado.TabIndex = 2;
+            this.gbListado.TabStop = false;
+            this.gbListado.Text = "Listado de Producción";
+
+            
+            this.dgvProduccion.AllowUserToAddRows = false;
+            this.dgvProduccion.AllowUserToDeleteRows = false;
+            this.dgvProduccion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProduccion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvProduccion.Location = new System.Drawing.Point(3, 16);
+            this.dgvProduccion.Name = "dgvProduccion";
+            this.dgvProduccion.ReadOnly = true;
+            this.dgvProduccion.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProduccion.Size = new System.Drawing.Size(494, 431);
+            this.dgvProduccion.TabIndex = 0;
+
+           
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 530);
+            this.Controls.Add(this.gbListado);
+            this.Controls.Add(this.gbDatos);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "PProduccionProductos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Producción de Productos";
+            this.Load += new System.EventHandler(this.PProduccionProductos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduccion)).EndInit();
+            this.gbDatos.ResumeLayout(false);
+            this.gbDatos.PerformLayout();
+            this.gbListado.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
     }
 }
--- PProductos.Designer.cs
+++ PProductos.Designer.cs
@@ -27,13 +27,147 @@
 
         private void InitializeComponent()
         {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.gbDatos = new System.Windows.Forms.GroupBox();
+            this.gbListado = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.gbDatos.SuspendLayout();
+            this.gbListado.SuspendLayout();
             this.SuspendLayout();
 
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "PProductos";
-            this.Load += new System.EventHandler(this.PProductos_Load);
-            this.ResumeLayout(false);
+            
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.Location = new System.Drawing.Point(250, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(200, 26);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Gestión de Productos";
 
+            
+            this.gbDatos.Controls.Add(this.lblId);
+            this.gbDatos.Controls.Add(this.txtId);
+            this.gbDatos.Controls.Add(this.lblDescripcion);
+            this.gbDatos.Controls.Add(this.txtDescripcion);
+            this.gbDatos.Controls.Add(this.btnInsertar);
+            this.gbDatos.Controls.Add(this.btnActualizar);
+            this.gbDatos.Controls.Add(this.btnEliminar);
+            this.gbDatos.Controls.Add(this.btnLimpiar);
+            this.gbDatos.Location = new System.Drawing.Point(20, 60);
+            this.gbDatos.Name = "gbDatos";
+            this.gbDatos.Size = new System.Drawing.Size(350, 250);
+            this.gbDatos.TabIndex = 1;
+            this.gbDatos.TabStop = false;
+            this.gbDatos.Text = "Datos del Producto";
+
+            
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(20, 30);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(21, 13);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "ID:";
+
+           
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(120, 27);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(200, 20);
+            this.txtId.TabIndex = 1;
+
+           
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(20, 70);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(66, 13);
+            this.lblDescripcion.TabIndex = 2;
+            this.lblDescripcion.Text = "Descripción:";
+
+          
+            this.txtDescripcion.Location = new System.Drawing.Point(120, 67);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(200, 20);
+            this.txtDescripcion.TabIndex = 3;
+
+          
+            this.btnInsertar.Location = new System.Drawing.Point(20, 120);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 30);
+            this.btnInsertar.TabIndex = 4;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+
+          
+            this.btnActualizar.Location = new System.Drawing.Point(110, 120);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 30);
+            this.btnActualizar.TabIndex = 5;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+
+          
+            this.btnEliminar.Location = new System.Drawing.Point(200, 120);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 30);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+
+         
+            this.btnLimpiar.Location = new System.Drawing.Point(20, 170);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 30);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+
+            
+            this.gbListado.Controls.Add(this.dgvProductos);
+            this.gbListado.Location = new System.Drawing.Point(390, 60);
+            this.gbListado.Name = "gbListado";
+            this.gbListado.Size = new System.Drawing.Size(450, 450);
+            this.gbListado.TabIndex = 2;
+            this.gbListado.TabStop = false;
+            this.gbListado.Text = "Listado de Productos";
+
+           
+            this.dgvProductos.AllowUserToAddRows = false;
+            this.dgvProductos.AllowUserToDeleteRows = false;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvProductos.Location = new System.Drawing.Point(3, 16);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.ReadOnly = true;
+            this.dgvProductos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProductos.Size = new System.Drawing.Size(444, 431);
+            this.dgvProductos.TabIndex = 0;
+
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 530);
+            this.Controls.Add(this.gbListado);
+            this.Controls.Add(this.gbDatos);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "PProductos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Productos";
+            this.Load += new System.EventHandler(this.PProductos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.gbDatos.ResumeLayout(false);
+            this.gbDatos.PerformLayout();
+            this.gbListado.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
     }
 }
